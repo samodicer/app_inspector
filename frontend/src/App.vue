61ec291c36d9f90b513ef8871a3acdad5241abf4
Samuel <54637954+samodicer@users.noreply.github.com>
2021-05-06 11:55:11 +0200

    linters update
    linters config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
-  components: {
-
-  }
-}
+  components: {},
+};
 </script>
 
-
-<style scoped>
-</style>
+<style scoped></style>
