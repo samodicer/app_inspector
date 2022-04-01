a9508a0e4b41a802421b4003366e242f1ebee71b
Samuel Dicer <dicersamuel@gmail.com>
2022-04-02 00:58:53 +0200

    Update axios-api.js

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getAPI = axios.create({
-  baseURL: 'https://app-inspector-api.herokuapp.com',
+  baseURL: 'https://appinspector-api.herokuapp.com',
   //baseURL: 'http://127.0.0.1:8000',
 });
 
