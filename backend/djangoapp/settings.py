368a9d4b3683844660b8a861c7cf7e8707f4f51d
Samuel Dicer <dicersamuel@gmail.com>
2022-04-04 18:50:59 +0200

    database changed
    database

--- backend/djangoapp/settings.py
+++ backend/djangoapp/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'djangoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
